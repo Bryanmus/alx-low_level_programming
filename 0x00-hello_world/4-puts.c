45e21e2e6ca2fa0e7fc88002945d5e213db3565c
Bryanmus <mutetibryan@gmail.com>
2023-02-16 14:59:16 -0800

    First C programming task commit status

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- main function block
+ *Return: zero since its an int function
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This the first C programming code
